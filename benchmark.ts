40146a593debe905248d75607b0360bd2bcc460e
Pavlo Batov <batovpasha@gmail.com>
2022-02-02 19:56:51 +0200

    test: add benchmarks suits for all implementations

--- benchmark.ts
+++ benchmark.ts
@@ -13,85 +13,151 @@ import {
   normalizeSpaces7,
 } from './lib';
 
-// const words5000 = fLI(5000, 'w');
+const words5000 = fLI(5000, 'w');
 const words50000 = fLI(50000, 'w');
-// const words500000 = fLI(500000, 'w');
-// const words5000000 = fLI(5000000, 'w');
+const words500000 = fLI(500000, 'w');
+const words5000000 = fLI(5000000, 'w');
 
 benny.suite(
-  '~330 kb',
-  benny.cycle(),
-  benny.add('normalize-space-x', () => {
-    normalizeSpaceX(words50000);
+  'normilizeSpaces1',
+  benny.add('~33 kb', () => {
+    normalizeSpaces1(words5000);
   }),
-  benny.cycle(),
-  benny.add('fast-impl-1', () => {
+  benny.add('~330 kb', () => {
     normalizeSpaces1(words50000);
   }),
-  benny.cycle(),
-  benny.add('fast-impl-2', () => {
-    normalizeSpaces2(words50000);
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces1(words500000);
   }),
-  benny.cycle(),
-  benny.add('fast-impl-3', () => {
-    normalizeSpaces3(words50000);
-  }),
-  benny.cycle(),
-  benny.add('fast-impl-4', () => {
-    normalizeSpaces4(words50000);
-  }),
-  benny.cycle(),
-  benny.add('fast-impl-5', () => {
-    normalizeSpaces5(words50000);
-  }),
-  benny.cycle(),
-  benny.add('fast-impl-6', () => {
-    normalizeSpaces6(words50000);
-  }),
-  benny.cycle(),
-  benny.add('fast-impl-7', () => {
-    normalizeSpaces7(words50000);
-  }),
-  benny.cycle(),
-  benny.add('normalize-space-x', () => {
-    normalizeSpaceX(words50000);
+  benny.add('~33 mb', () => {
+    normalizeSpaces1(words5000000);
   }),
   benny.cycle(),
   benny.complete()
 );
 
-// benny.suite(
-//   '@shelf/fast-normalize-spaces',
-//   benny.add('~33 kb', () => {
-//     normalizeSpaces(words5000);
-//   }),
-//   benny.add('~330 kb', () => {
-//     normalizeSpaces(words50000);
-//   }),
-//   benny.add('~3.3 mb', () => {
-//     normalizeSpaces(words500000);
-//   }),
-//   benny.add('~33 mb', () => {
-//     normalizeSpaces(words5000000);
-//   }),
-//   benny.cycle(),
-//   benny.complete()
-// );
-//
-// benny.suite(
-//   'normalize-space-x',
-//   benny.add('~33 kb', () => {
-//     normalizeSpaceX(words5000);
-//   }),
-//   benny.add('~330 kb', () => {
-//     normalizeSpaceX(words50000);
-//   }),
-//   benny.add('~3.3 mb', () => {
-//     normalizeSpaceX(words500000);
-//   }),
-//   benny.add('~33 mb', () => {
-//     normalizeSpaceX(words5000000);
-//   }),
-//   benny.cycle(),
-//   benny.complete()
-// );
+benny.suite(
+  'normilizeSpaces2',
+  benny.add('~33 kb', () => {
+    normalizeSpaces2(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces2(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces2(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces2(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normilizeSpaces3',
+  benny.add('~33 kb', () => {
+    normalizeSpaces3(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces3(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces3(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces3(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normilizeSpaces4',
+  benny.add('~33 kb', () => {
+    normalizeSpaces4(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces4(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces4(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces4(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normilizeSpaces5',
+  benny.add('~33 kb', () => {
+    normalizeSpaces5(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces5(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces5(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces5(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normilizeSpaces6',
+  benny.add('~33 kb', () => {
+    normalizeSpaces6(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces6(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces6(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces6(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normilizeSpaces7',
+  benny.add('~33 kb', () => {
+    normalizeSpaces7(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaces7(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaces7(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaces7(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'normalize-space-x',
+  benny.add('~33 kb', () => {
+    normalizeSpaceX(words5000);
+  }),
+  benny.add('~330 kb', () => {
+    normalizeSpaceX(words50000);
+  }),
+  benny.add('~3.3 mb', () => {
+    normalizeSpaceX(words500000);
+  }),
+  benny.add('~33 mb', () => {
+    normalizeSpaceX(words5000000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
